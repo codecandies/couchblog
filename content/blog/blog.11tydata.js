9cd5a3918e9d94d5315331bc68cc6aeb3705bdac
Nico Brünjes <nico.bruenjes@gmail.com>
2024-06-23 12:34:52 +0200

    Drafts: Add possibility for drafts in content folder
    Use `draft: true` or date in the future for drafting. Will be uploaded to github. Articles with future dates will appear when build after that date.

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -1,7 +1,25 @@
-module.exports = {
-  tags: [
-    "blog"
-  ],
-  "layout": "layouts/blog.njk",
-  "permalink": "blog/{{ date | pathDate }}/{{ title | slugify }}/index.html"
+require('dotenv').config();
+
+const isDevEnv = process.env.ELEVENTY_ENV === 'development';
+const todaysDate = new Date();
+
+function showDraft(data) {
+	const isDraft = 'draft' in data && data.draft !== false;
+	const isFutureDate = data.page.date > todaysDate;
+	return isDevEnv || (!isDraft && !isFutureDate);
+}
+
+
+module.exports = ()=> {
+  return {
+    tags: [
+      "blog"
+    ],
+    "layout": "layouts/blog.njk",
+    "permalink": "blog/{{ date | pathDate }}/{{ title | slugify }}/index.html",
+		eleventyComputed: {
+			eleventyExcludeFromCollections: data => showDraft(data) ? data.eleventyExcludeFromCollections : true,
+			permalink: data => showDraft(data) ? data.permalink : false,
+		}
+	}
 };
