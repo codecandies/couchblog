6749f27c558af3d9a111cd9c452c6f6aadac39db
Nico Bruenjes <nico.bruenjes@mailbox.org>
2026-01-06 19:02:32 +0100

    feat: add comment form

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -4,11 +4,16 @@ const isDevEnv = process.env.ELEVENTY_ENV === 'development';
 const todaysDate = new Date();
 
 function showDraft(data) {
-	const isDraft = 'draft' in data && data.draft !== false;
-	const isFutureDate = data.page.date > todaysDate;
-	return isDevEnv || (!isDraft && !isFutureDate);
+  const isDraft = 'draft' in data && data.draft !== false;
+  const isFutureDate = data.page.date > todaysDate;
+  return isDevEnv || (!isDraft && !isFutureDate);
 }
 
+function showComments(data) {
+  const twoMonthsAgo = new Date();
+  twoMonthsAgo.setMonth(twoMonthsAgo.getMonth() - 2);
+  return data.page.date >= twoMonthsAgo;
+}
 
 module.exports = ()=> {
   return {
@@ -17,9 +22,10 @@ module.exports = ()=> {
     ],
     "layout": "layouts/blog.njk",
     "permalink": "blog/{{ date | pathDate }}/{{ title | slugify }}/index.html",
-		eleventyComputed: {
-			eleventyExcludeFromCollections: data => showDraft(data) ? data.eleventyExcludeFromCollections : true,
-			permalink: data => showDraft(data) ? data.permalink : false,
-		}
-	}
+    eleventyComputed: {
+      eleventyExcludeFromCollections: data => showDraft(data) ? data.eleventyExcludeFromCollections : true,
+      permalink: data => showDraft(data) ? data.permalink : false,
+      showComments: data => showComments(data)
+    }
+  }
 };
